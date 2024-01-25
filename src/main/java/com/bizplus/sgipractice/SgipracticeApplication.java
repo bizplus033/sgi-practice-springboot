f85a22736526a7656a1210cb6c62c820150fecb8
bizplus033 <bizplus033@naver.com>
2024-01-25 09:47:18 +0900

    Initial commit

---
+++ src/main/java/com/bizplus/sgipractice/SgipracticeApplication.java
@@ -0,0 +1,13 @@
+package com.bizplus.sgipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgipracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgipracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bizplus/sgipractice/SgipracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizplus.sgipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgipracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
