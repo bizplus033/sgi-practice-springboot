389ab0ca79b14900a2d0daa146b585b6d22b0327
bizplus033 <bizplus033@naver.com>
2024-01-26 10:34:47 +0900

    [feature/base-entity] 설정 파일

---
+++ src/main/java/com/bizplus/sgipractice/global/config/AuditorAwareImpl.java
@@ -0,0 +1,23 @@
+package com.bizplus.sgipractice.global.config;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+@RequiredArgsConstructor
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    private final HttpServletRequest httpServletRequest;
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        String modifiedBy = httpServletRequest.getRequestURI();
+        if (!StringUtils.hasText(modifiedBy)) {
+            modifiedBy = "unknown";
+        }
+        return Optional.of(modifiedBy);
+    }
+}
