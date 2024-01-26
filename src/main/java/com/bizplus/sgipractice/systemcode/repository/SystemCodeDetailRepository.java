372302255d45f4d8544c303574a749c043c6fc3b
bizplus033 <bizplus033@naver.com>
2024-01-26 11:09:24 +0900

    [feature/system-code] repository

---
+++ src/main/java/com/bizplus/sgipractice/systemcode/repository/SystemCodeDetailRepository.java
@@ -0,0 +1,9 @@
+package com.bizplus.sgipractice.systemcode.repository;
+
+import com.bizplus.sgipractice.systemcode.entity.SystemCodeDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SystemCodeDetailRepository extends JpaRepository<SystemCodeDetail, Long> {
+
+
+}
---
+++ src/main/java/com/bizplus/sgipractice/systemcode/repository/SystemCodeRepository.java
@@ -0,0 +1,7 @@
+package com.bizplus.sgipractice.systemcode.repository;
+
+import com.bizplus.sgipractice.systemcode.entity.SystemCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SystemCodeRepository extends JpaRepository<SystemCode, Long> {
+}
