cf5be50628c0cff5d96540dd38f1e9b650845ce7
bizplus033 <bizplus033@naver.com>
2024-01-29 09:39:13 +0900

    [feature/system-code] BaseTimeEntity - > @EntityListeners(value = {AuditingEntityListener.class}) 수정, DTO에 setter와 기본생성자 추가

--- src/main/java/com/bizplus/sgipractice/common/domain/BaseTimeEntity.java
+++ src/main/java/com/bizplus/sgipractice/common/domain/BaseTimeEntity.java
@@ -3,13 +3,14 @@ package com.bizplus.sgipractice.common.domain;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@EntityListeners(value = {ArithmeticException.class})
+@EntityListeners(value = {AuditingEntityListener.class})
 @MappedSuperclass
 @Getter
 public abstract class BaseTimeEntity {
--- src/main/java/com/bizplus/sgipractice/systemcode/dto/SystemCodeDetailRequest.java
+++ src/main/java/com/bizplus/sgipractice/systemcode/dto/SystemCodeDetailRequest.java
@@ -3,8 +3,12 @@ package com.bizplus.sgipractice.systemcode.dto;
 import com.bizplus.sgipractice.systemcode.entity.SystemCode;
 import com.bizplus.sgipractice.systemcode.entity.SystemCodeDetail;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 public class SystemCodeDetailRequest {
     private String name;
     private Long systemCodeId;
