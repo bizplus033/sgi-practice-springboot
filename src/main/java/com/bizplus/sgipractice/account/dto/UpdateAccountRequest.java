db9dd1c2d4ab5496a1301f94ce824b4422be342c
bizplus033 <bizplus033@naver.com>
2024-02-16 15:44:59 +0900

    [feature/account-create] UpdateAccountRequest - 미리 만들어둠

---
+++ src/main/java/com/bizplus/sgipractice/account/dto/UpdateAccountRequest.java
@@ -0,0 +1,27 @@
+package com.bizplus.sgipractice.account.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateAccountRequest {
+    private Long id;
+    private String code;
+    private String type;
+    private String name;
+    private String businessNumber;
+    private String ceo;
+    private String businessType;
+    private String businessCategory;
+    private String tel;
+    private String fax;
+    private String email;
+    private String zipCode;
+    private String address;
+    private String addressDetail;
+    private String etc;
+}
